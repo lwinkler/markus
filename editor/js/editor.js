b68915e8b0c5f2cb834bef04d95b273da26d2ca8
Laurent Winkler <laurent.winkler@bluewin.ch>
2013-09-08 19:59:47 +0200

    - Editor: cleanup

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -66,43 +66,7 @@ var xmlProject = null;
 				activeClass:"dragActive"
 			};
 
-			//
-			// first example endpoint.  it's a 25x21 rectangle (the size is provided in the 'style' arg to the Endpoint),
-			// and it's both a source and target.  the 'scope' of this Endpoint is 'exampleConnection', meaning any connection
-			// starting from this Endpoint is of type 'exampleConnection' and can only be dropped on an Endpoint target
-			// that declares 'exampleEndpoint' as its drop scope, and also that
-			// only 'exampleConnection' types can be dropped here.
-			//
-			// the connection style for this endpoint is a Bezier curve (we didn't provide one, so we use the default), with a lineWidth of
-			// 5 pixels, and a gradient.
-			//
-			// there is a 'beforeDrop' interceptor on this endpoint which is used to allow the user to decide whether
-			// or not to allow a particular connection to be established.
-			//
-			/*var exampleColor = "#00f";
-			var exampleEndpoint = {
-				endpoint:"Rectangle",
-				paintStyle:{ width:25, height:21, fillStyle:exampleColor },
-				isSource:true,
-				reattach:true,
-				scope:"blue rectangle",
-				connectorStyle : {
-					gradient:{stops:[[0, exampleColor], [0.5, "#09098e"], [1, exampleColor]]},
-					lineWidth:5,
-					strokeStyle:exampleColor,
-					dashstyle:"2 2"
-				},
-				isTarget:true,
-				beforeDrop:function(params) { 
-					return confirm("Connect " + params.sourceId + " to " + params.targetId + "?"); 
-				},				
-				dropOptions : dropOptions
-			};*/			
-
-			//
-			// the second example uses a Dot of radius 15 as the endpoint marker, is both a source and target,
-			// and has scope 'exampleConnection2'.
-			//
+			// Configuration of endpoints for jsPlumb
 			var colorDefault = "#316b31";
 			
 			var pointsColor = {
@@ -118,7 +82,6 @@ var xmlProject = null;
 				isSource:false,
 				scope:"default",
 				connectorStyle:{ strokeStyle:colorDefault, lineWidth:2 },
-				// connector: ["Bezier", { curviness:63 } ],
 				maxConnections:1,
 				isTarget:true,
 				beforeDrop:function(params) { 
@@ -144,73 +107,13 @@ var xmlProject = null;
 				isSource:true,
 				scope:"default",
 				connectorStyle:{ strokeStyle:colorDefault, lineWidth:2 },
-				// connector: ["Bezier", { curviness:63 } ],
+				connector: ["Bezier", { curviness:63 } ],
 				// connector: "Straight",
+				// connector: "Flowchart",
 				maxConnections:99,
 				isTarget:false,
 				dropOptions : dropOptions
 			};
-			//
-			// the third example uses a Dot of radius 17 as the endpoint marker, is both a source and target, and has scope
-			// 'exampleConnection3'.  it uses a Straight connector, and the Anchor is created here (bottom left corner) and never
-			// overriden, so it appears in the same place on every element.
-			//
-			// this example also demonstrates the beforeDetach interceptor, which allows you to intercept 
-			// a connection detach and decide whether or not you wish to allow it to proceed.
-			//			
-			/*var example3Color = "rgba(229,219,61,0.5)";
-			var exampleEndpoint3 = {
-				endpoint:["Dot", {radius:17} ],
-				anchor:"BottomLeft",
-				paintStyle:{ fillStyle:example3Color, opacity:0.5 },
-				isSource:true,
-				scope:'yellow dot',
-				connectorStyle:{ strokeStyle:example3Color, lineWidth:4 },
-				connector : "Straight",
-				isTarget:true,
-				dropOptions : dropOptions,
-				beforeDetach:function(conn) { 
-					return confirm("Detach connection?"); 
-				},
-				onMaxConnections:function(info) {
-					alert("Cannot drop connection " + info.connection.id + " : maxConnections has been reached on Endpoint " + info.endpoint.id);
-				}
-			};*/
-/*
-			// setup some empty endpoints.  again note the use of the three-arg method to reuse all the parameters except the location
-			// of the anchor (purely because we want to move the anchor around here; you could set it one time and forget about it though.)
-			var e1 = jsPlumb.addEndpoint('window1', { anchor:[0.5, 1, 0, 1] }, exampleEndpoint2);
-
-			// setup some DynamicAnchors for use with the blue endpoints			
-			// and a function to set as the maxConnections callback.
-			var anchors = [[1, 0.2, 1, 0], [0.8, 1, 0, 1], [0, 0.8, -1, 0], [0.2, 0, 0, -1] ],
-				maxConnectionsCallback = function(info) {
-					alert("Cannot drop connection " + info.connection.id + " : maxConnections has been reached on Endpoint " + info.endpoint.id);
-				};
-				
-			var e1 = jsPlumb.addEndpoint("window1", { anchor:anchors }, exampleEndpoint);
-			// you can bind for a maxConnections callback using a standard bind call, but you can also supply 'onMaxConnections' in an Endpoint definition - see exampleEndpoint3 above.
-			e1.bind("maxConnections", maxConnectionsCallback);
-
-			var e2 = jsPlumb.addEndpoint('window2', { anchor:[0.5, 1, 0, 1] }, exampleEndpoint);
-			// again we bind manually. it's starting to get tedious.  but now that i've done one of the blue endpoints this way, i have to do them all...
-			e2.bind("maxConnections", maxConnectionsCallback);
-			jsPlumb.addEndpoint('window2', { anchor:"RightMiddle" }, exampleEndpoint2);
-
-			var e3 = jsPlumb.addEndpoint("window3", { anchor:[0.25, 0, 0, -1] }, exampleEndpoint);
-			e3.bind("maxConnections", maxConnectionsCallback);
-			jsPlumb.addEndpoint("window3", { anchor:[0.75, 0, 0, -1] }, exampleEndpoint2);
-
-			var e4 = jsPlumb.addEndpoint("window4", { anchor:[1, 0.5, 1, 0] }, exampleEndpoint);
-			e4.bind("maxConnections", maxConnectionsCallback);			
-			jsPlumb.addEndpoint("window4", { anchor:[0.25, 0, 0, -1] }, exampleEndpoint2);
-
-			// make .window divs draggable
-			jsPlumb.draggable($(".window"));
-
-			// add endpoint of type 3 using a selector. 
-			jsPlumb.addEndpoint($(".window"), exampleEndpoint3);
-*/
 			//================================================================================
 			// Functions and utilities
 			//================================================================================
@@ -354,7 +257,7 @@ var xmlProject = null;
 			function createNewModuleInConfig(type){
 
 				var id = maxIdModules;
-				var xmlInstance = $("<module/>", xmlProject).appendTo($(xmlProject).find("application"));
+				var xmlInstance = $("<module/>", xmlProject).appendTo($(xmlProject));
 				xmlInstance.attr({
 					id: id,
 					name: type + id
@@ -414,11 +317,12 @@ var xmlProject = null;
 
 
 				// Load the xml file
+				jsPlumb.detachEveryConnection();
 				$("#main > .window").remove();
 				$("#main > ._jsPlumb_endpoint").remove();
 				xmlProject = $(xml).find("application");
 
-				xmlProject.find('application > module').each(function(index){
+				xmlProject.find('module').each(function(index){
 					var type = $(this).find('parameters > param[name="class"]').text();
 
 					$(this).find('inputs > input').each(function(){
@@ -438,7 +342,7 @@ var xmlProject = null;
 				});
 
 				// Link inputs-outputs
-				xmlProject.find('application > module').each(function(index){
+				xmlProject.find('module').each(function(index){
 					$(this).find('inputs > input').each(function(){
 
 						// rebuild connections
@@ -447,7 +351,7 @@ var xmlProject = null;
 						if(moduleid != "" && outputid != "") {
 							jsPlumb.connect({
 								sourceEndpoint: $(this).data('gui')[0],
-								targetEndpoint: xmlProject.find('application > module[id="' + moduleid + '"] > outputs > output[id="' + outputid + '"]').data('gui')[0],
+								targetEndpoint: xmlProject.find('module[id="' + moduleid + '"] > outputs > output[id="' + outputid + '"]').data('gui')[0],
 								// type:"basic"
 							});
 						}
@@ -490,28 +394,6 @@ var xmlProject = null;
 
 
 			if (!_initialised) {
-				/*$(".hide").click(function() {
-					jsPlumb.toggleVisible($(this).attr("rel"));
-				});
-	
-				$(".drag").click(function() {
-					var s = jsPlumb.toggleDraggable($(this).attr("rel"));
-					$(this).html(s ? 'disable dragging' : 'enable dragging');
-					if (!s)
-						$("#" + $(this).attr("rel")).addClass('drag-locked');
-					else
-						$("#" + $(this).attr("rel")).removeClass('drag-locked');
-					$("#" + $(this).attr("rel")).css("cursor", s ? "pointer" : "default");
-				});
-	
-				$(".detach").click(function() {
-					jsPlumb.detachAllConnections($(this).attr("rel"));
-				});
-	
-				$("#clear").click(function() { 
-					jsPlumb.detachEveryConnection();
-					showConnectionInfo("");
-				});*/
 
 				// Populate select modules with existing modules
 
@@ -534,7 +416,7 @@ var xmlProject = null;
 				$("#downloadProject").click(function() {
 
 					// Include position information in the xml
-    					xmlProject.find('application > module').each(function(){
+    					xmlProject.find('module').each(function(){
 						var window = $(this).data('gui');
 						var pos = window.offset();
 						$(this).find('uiobject').remove();
@@ -569,9 +451,12 @@ var xmlProject = null;
 				$("#loadProjectFile").click(function (){
 					loadProjectFile($("#selectProjectFile").val());
 				});
+				$("#deleteAll").click(function() {
+					
+					
+				});
 
 				// Load an empty project
-				//xmlProject = $(loadXML("editor/xml/EmptyProject.xml"));
 				xmlProject = $(document.implementation.createDocument(null, "application", null));
 				_initialised = true;
 			}
