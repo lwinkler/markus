30636b8103a276af2c9f5cbc21a5b979c34b4c00
Laurent Winkler <laurent.winkler@bluewin.ch>
2013-09-27 18:03:13 +0200

    - Add possibility to make scatter plots

--- tools/plot_object_feature.py
+++ tools/plot_object_feature.py
@@ -13,17 +13,37 @@ import math
 import datetime
 
 
-def plot_object(objName):
+def plot_object(objName, features, plotType):
 # Filter data per feature
-	vect = np.where((data['object'] == objName) & (data['feature'] == feature))
-	result = data[vect[0]]
-	time1 = [ datetime.datetime.fromtimestamp(ms/1000.0) for ms in result['time']] 
-	plt.errorbar(time1,result['value'],yerr=[math.sqrt(i) for i in result['sqVariance']], fmt="-o") # , label='value with variance') #, errorevery=5)
-	plt.plot(time1, result['mean']) # , color='r', label='mean')
-# plt.plot(time1, result['min'], color='g', label='min')
-# plt.plot(time1, result['max'], color='g', label='max')
-# plt.plot(time1, result['initial'], color='y', label='initial')
-# plt.plot(time1, result['nbSamples'], color='pink', label='nbSamples')
+
+	if plotType == 'linear':
+		vect = np.where((data['object'] == objName) & (data['feature'] == features[0]))
+		result = data[vect[0]]
+		time1 = [ datetime.datetime.fromtimestamp(ms/1000.0) for ms in result['time']] 
+		plt.errorbar(time1, result['value'], yerr=[math.sqrt(i) for i in result['sqVariance']], fmt="-o") # , label='value with variance') #, errorevery=5)
+		plt.plot(time1, result['mean']) # , color='r', label='mean')
+		# plt.plot(time1, result['min'], color='g', label='min')
+		# plt.plot(time1, result['max'], color='g', label='max')
+		# plt.plot(time1, result['initial'], color='y', label='initial')
+		# plt.plot(time1, result['nbSamples'], color='pink', label='nbSamples')
+
+		# Set plot
+		ax1.set_title("Evolution of " + features[0] + " for " + objName)
+		ax1.set_xlabel('time')
+		ax1.set_ylabel(features[0])
+	elif plotType == 'scatter':
+		vect1 = np.where((data['object'] == objName) & (data['feature'] == features[0]))
+		result1 = data[vect1[0]]
+		vect2 = np.where((data['object'] == objName) & (data['feature'] == features[1]))
+		result2 = data[vect2[0]]
+		plt.scatter(result1['value'], result2['value']) # , color='r', label='mean')
+
+		# Set plot
+		ax1.set_title("Evolution of " + features[0] + " vs " + features[1])
+		ax1.set_xlabel(features[0])
+		ax1.set_ylabel(features[1])
+
+
 
 
 
@@ -31,12 +51,16 @@ def plot_object(objName):
 
 # check arguments
 if len(sys.argv) < 4:
-	print "usage: " + sys.argv[0] + " file object feature"
+	print "usage: " + sys.argv[0] + " plot_type file object feature(s)"
+	print "ex: " + sys.argv[0] + " linear file object feature"
+	print "ex: " + sys.argv[0] + " scatter file object feature1 feature2"
+
 	exit()
 
 fileName = sys.argv[1]
-objName  = sys.argv[2]
-feature  = sys.argv[3]
+plotType = sys.argv[2]
+objName  = sys.argv[3]
+features  = sys.argv[4:]
 
 data = np.genfromtxt(fileName, delimiter='\t', names=True, dtype=None)
 
@@ -61,11 +85,8 @@ else:
 # Plot all objects
 for name in objNames:
 	print "plot_object " + name
-	plot_object(name)
+	plot_object(name, features, plotType)
 
-ax1.set_title("Evolution of " + feature + " for " + objName)
-ax1.set_xlabel('time')
-ax1.set_ylabel(feature)
 
 #ax1.plot(x,y, c='r', label='the data')
 
