f0786a4d16faa678cbc575bca4310540056750a2
Laurent Winkler <laurent.winkler@bluewin.ch>
2013-11-11 23:58:26 +0100

    - Bugfix CMakeLists

--- tools/version.sh
+++ tools/version.sh
@@ -4,12 +4,12 @@ echo -n "git branch: ";
 git rev-parse --abbrev-ref HEAD
 echo -n "git commit: ";
 git log --pretty=oneline -1 --abbrev-commit HEAD | cat
-echo -n "git describe tags: ";
-git describe --tags HEAD | cat
-echo -n "Built on:   "
-date
-echo -n "date:       ";
+echo -n "git descr : ";
+git describe --all --long HEAD
+echo -n "Built on  : ";
 uname -a
+echo -n "Date      : ";
+date
 
 echo
 echo "Last 5 commits"
