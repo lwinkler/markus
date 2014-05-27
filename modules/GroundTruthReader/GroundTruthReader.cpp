5097377a54ac919759e27d7f064c3e08738aab8c
Olivia Manz <olivia.manz@gmail.com>
2014-05-27 11:42:14 +0200

    Add StreamObject and Event to GroundTruth module

--- modules/GroundTruthReader/GroundTruthReader.cpp
+++ modules/GroundTruthReader/GroundTruthReader.cpp
@@ -23,7 +23,9 @@
 
 #include "GroundTruthReader.h"
 #include "StreamImage.h"
+#include "StreamObject.h"
 #include "StreamState.h"
+#include "StreamEvent.h"
 #include "StreamDebug.h"
 #include "util.h"
 
@@ -40,9 +42,12 @@ GroundTruthReader::GroundTruthReader(const ConfigReader& x_configReader):
 {
 	mp_annotationReader = NULL;
 	AddInputStream(0, new StreamImage("input",  m_input, *this, "Video input"));
+	AddInputStream(1, new StreamObject("input object",  m_objects, *this, "Incoming objects"));
 
 	// AddOutputStream(0, new StreamImage("input", m_input,  *this, 	"Copy of the input stream"));
 	AddOutputStream(0, new StreamState("state", m_state,  *this, 	"State read from the annotation file"));
+	AddOutputStream(1, new StreamObject("output object",  m_objects, *this, "Outcoming objects"));
+	AddOutputStream(2, new StreamEvent( "change_gt", m_event,     *this,   "State of ground truth changed"));
 
 #ifdef MARKUS_DEBUG_STREAMS
 	m_debug = Mat(Size(m_param.width, m_param.height), CV_8UC3);
@@ -59,6 +64,7 @@ void GroundTruthReader::Reset()
 {
 	Module::Reset();
 	m_state    = false;
+	m_oldState = false;
 
 	CLEAN_DELETE(mp_annotationReader);
 	mp_annotationReader = new AnnotationFileReader();
@@ -67,26 +73,33 @@ void GroundTruthReader::Reset()
 
 void GroundTruthReader::ProcessFrame()
 {
+	m_event.Empty();
 	m_state = false;
+#ifdef MARKUS_DEBUG_STREAMS
+	m_debug.setTo(0);
+#endif
 
 	// Interpret the current state
 	string text = mp_annotationReader->ReadAnnotationForTimeStamp(m_currentTimeStamp);
 	if(text != "")
 	{
 		m_state = text.find(m_param.pattern) != string::npos; // TODO: Improve this with regexp
+
 #ifdef MARKUS_DEBUG_STREAMS
-		m_debug.setTo(0);
 		Scalar color = m_state ? Scalar(0, 255, 0) : Scalar(0, 0, 255); 
 		putText(m_debug, text, Point(40, 40),  FONT_HERSHEY_COMPLEX_SMALL, 0.8, color);
 #endif
 	}
-	else
+
+	// annotate stream objects
+	for(vector<Object>::iterator it = m_objects.begin() ; it != m_objects.end() ; it++)
 	{
-		m_state = false;
-#ifdef MARKUS_DEBUG_STREAMS
-		m_debug.setTo(0);
-#endif
+		it->AddFeature("gt",m_state);
+	}
+
+	if (m_oldState != m_state)
+	{
+		m_event.Raise("Ground Truth changed");
+		m_oldState = m_state;
 	}
 }
-
-
