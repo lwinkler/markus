4d4db404e320655340e38e8ef1b906e0a54151d5
Laurent Winkler <laurent.winkler@bluewin.ch>
2014-06-12 13:03:21 +0200

    Fix release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
 
 
-MARKUS_RELEASE="v0.0.3_"`date "+%Y-%m-%d_%H%M%S"`
+
+function deploy {
+	read -r -p "Do you want to deploy version "$TARGET" ? [y/N] " response
+	case $response in
+		[yY][eE][sS]|[yY]) 
+
+		scp -r ${TARGET} vp@smartaid.quality:~
+		scp -r ${TARGET} /mnt/releases/Morphean/VideoAID
+
+		;;
+		*)
+		;;
+	esac
+}
+
+MARKUS_RELEASE="v0.0.3_"
+#`date "+%Y-%m-%d_%H%M%S"`
 
 read -r -p "Do you want to release version "$MARKUS_RELEASE" ? [y/N] " response
 case $response in
@@ -19,8 +35,7 @@ rm -rf ${TARGET}
 mkdir ${TARGET}
 scp -r markus projects2/FallDetectionFromVP.xml modules2/FilterPython/vignettes log4cxx.min.xml ${TARGET}
 
-scp -r ${TARGET} vp@smartaid.quality:~
-scp -r ${TARGET} /mnt/releases/Morphean/VideoAID              
+deploy
 
 
 # motion
@@ -30,6 +45,5 @@ rm -rf ${TARGET}
 mkdir ${TARGET}
 scp -r markus projects2/MotionDetectorFromVP.xml log4cxx.min.xml ${TARGET}
 
-scp -r ${TARGET} vp@smartaid.quality:~
-scp -r ${TARGET} /mnt/releases/Morphean/VideoAID
 
+deploy
