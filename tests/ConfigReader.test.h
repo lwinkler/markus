85b8263f08bd8e10c7d01ca1d2e98105240f8c07
Laurent Winkler <laurent.winkler@bluewin.ch>
2015-01-04 02:31:13 +0100

    Transfert CxxTest for Features

--- tests/ConfigReader.test.h
+++ tests/ConfigReader.test.h
@@ -1,3 +1,28 @@
+/*----------------------------------------------------------------------------------
+*
+*    MARKUS : a manager for video analysis modules
+* 
+*    author : Laurent Winkler <lwinkler888@gmail.com>
+* 
+* 
+*    This file is part of Markus.
+*
+*    Markus is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Markus is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with Markus.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------------*/
+#ifndef TEST_CONFIGREADER_H
+#define TEST_CONFIGREADER_H
+
 #include <cxxtest/TestSuite.h>
 #include <log4cxx/xml/domconfigurator.h>
 
@@ -19,6 +44,8 @@ protected:
 public:
 	void setUp()
 	{
+		// log4cxx::xml::DOMConfigurator::configure("tests/log4cxx.xml");
+
 		m_conf1 = new ConfigReader("testing/config1.xml");
 		createEmptyConfigFile("/tmp/config_empty.xml");
 		m_conf2 = new ConfigReader("/tmp/config_empty.xml");
@@ -37,6 +64,7 @@ public:
 	/// Test new syntax
 	void testSyntax()
 	{
+		LOG_TEST(m_logger, "\n# Test on the syntax of configurations");
 		ConfigReader conf(*m_conf3);
 		conf.RefSubConfig("t1", true).RefSubConfig("t2", true).RefSubConfig("t3", "name", "bla", true).SetValue("333");
 		m_conf3->SaveToFile("testing/tmp/test3.xml");
@@ -92,7 +120,7 @@ public:
 	/// Generate a config from an empty file and test
 	void testGenerate()
 	{
-		LOG_TEST(m_logger, "# Test the generation of configurations");
+		LOG_TEST(m_logger, "\n# Test the generation of configurations");
 		ConfigReader appConf = m_conf2->RefSubConfig("application", true);
 		appConf.RefSubConfig("aaa", "name", "nameX", true)
 			.RefSubConfig("bbb", "name", "nameY", true)
@@ -108,4 +136,5 @@ public:
 	}
 };
 
-log4cxx::LoggerPtr ConfigReaderTestSuite::m_logger(log4cxx::Logger::getLogger("ConfigReaderTest"));
+log4cxx::LoggerPtr ConfigReaderTestSuite::m_logger(log4cxx::Logger::getLogger("ConfigReaderTestSuite"));
+#endif
