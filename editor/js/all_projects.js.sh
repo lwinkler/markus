559a9f9fa182534764148d73c9063e006f68beae
Laurent Winkler <laurent.winkler@bluewin.ch>
2015-01-13 15:48:34 +0100

    Fix a bug with update_projects_list

--- editor/js/all_projects.js.sh
+++ editor/js/all_projects.js.sh
@@ -4,5 +4,5 @@
 
 echo "var availableProjectsNames = ["
 find projects*/ -maxdepth 1  -name "*.xml" | sort | awk {'print "\""$1"\","'}
-find testing/projects*/  -name "*.xml" | sort | awk {'print "\""$1"\","'}
+# find tests/projects*/  -name "*.xml" | sort | awk {'print "\""$1"\","'}
 echo "];"
