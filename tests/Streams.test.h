1576d16f077c85efa5b42f4cef1aab73e489810d
Laurent Winkler <laurent.winkler@bluewin.ch>
2015-12-07 01:00:15 +0100

    Basic test case for streams

--- tests/Streams.test.h
+++ tests/Streams.test.h
@@ -50,9 +50,20 @@ class StreamsTestSuite : public CxxTest::TestSuite
 	{
 	public:
 		FakeModule(ParameterStructure& xr_params) :
-		Module(xr_params),
-		m_param(dynamic_cast<Parameters&>(xr_params))
+			Module(xr_params),
+			m_param(dynamic_cast<Parameters&>(xr_params)),
+			m_image(cv::Size(m_param.width, m_param.height), m_param.type)
 		{
+			m_objs.resize(10);
+			m_evts.resize(14);
+			m_images.resize(9);
+
+			for(auto& im : m_images)
+			{
+				im = cv::Mat(cv::Size(m_param.width, m_param.height), m_param.type);
+			}
+
+			TS_ASSERT(GetWidth() * GetHeight() > 0);
 			AddInputStream( 0, new StreamObject("object", m_obj, *this, "Test stream"));
 			AddInputStream( 1, new StreamEvent("event", m_evt, *this, "Test stream"));
 			AddInputStream( 2, new StreamImage("image", m_image, *this, "Test stream"));
@@ -62,9 +73,9 @@ class StreamsTestSuite : public CxxTest::TestSuite
 			AddInputStream( 6, new StreamNum<float>("float", m_float, *this, "Test stream"));
 			AddInputStream( 7, new StreamNum<double>("double", m_double, *this, "Test stream"));
 
-			AddInputStream( 0, new MultiStreamT<vector<Object>>("object", m_objs, *this, "Test stream"));
-			AddInputStream( 1, new MultiStreamT<Event>("event", m_evts, *this, "Test stream"));
-			AddInputStream( 2, new MultiStreamT<cv::Mat>("image", m_images, *this, "Test stream"));
+			AddInputStream( 8, new MultiStreamT<vector<Object>>("object", m_objs, *this, "Test stream"));
+			AddInputStream( 9, new MultiStreamT<Event>("event", m_evts, *this, "Test stream"));
+			AddInputStream(10, new MultiStreamT<cv::Mat>("image", m_images, *this, "Test stream"));
 			// AddInputStream( 3, new MultiStreamT<bool>("bool", m_bools, *this, "Test stream"));
 			// AddInputStream( 4, new MultiStreamT<int>("int", m_ints, *this, "Test stream"));
 			// AddInputStream( 5, new MultiStreamT<uint>("uint", m_uints, *this, "Test stream"));
@@ -80,9 +91,9 @@ class StreamsTestSuite : public CxxTest::TestSuite
 			AddOutputStream( 6, new StreamNum<float>("float", m_float, *this, "Test stream"));
 			AddOutputStream( 7, new StreamNum<double>("double", m_double, *this, "Test stream"));
 
-			AddOutputStream( 0, new MultiStreamT<vector<Object>>("object", m_objs, *this, "Test stream"));
-			AddOutputStream( 1, new MultiStreamT<Event>("event", m_evts, *this, "Test stream"));
-			AddOutputStream( 2, new MultiStreamT<cv::Mat>("image", m_images, *this, "Test stream"));
+			AddOutputStream( 8, new MultiStreamT<vector<Object>>("object", m_objs, *this, "Test stream"));
+			AddOutputStream( 9, new MultiStreamT<Event>("event", m_evts, *this, "Test stream"));
+			AddOutputStream(10, new MultiStreamT<cv::Mat>("image", m_images, *this, "Test stream"));
 			// AddOutputStream( 3, new MultiStreamT<bool>("bool", m_bools, *this, "Test stream"));
 			// AddOutputStream( 4, new MultiStreamT<int>("int", m_ints, *this, "Test stream"));
 			// AddOutputStream( 5, new MultiStreamT<uint>("uint", m_uints, *this, "Test stream"));
@@ -94,10 +105,15 @@ class StreamsTestSuite : public CxxTest::TestSuite
 		MKCLASS("FakeModule")
 		MKDESCR("Output video stream with additional object streams")
 		void Reset(){}
-		virtual void ProcessFrame(){}
+		virtual void ProcessFrame()
+		{
+			for(auto& stream : m_inputStreams)
+				stream.second->Randomize(seed);
+		}
 
 	private:
-		const ParameterStructure& m_param;
+		const Module::Parameters& m_param;
+		uint seed = 6345345;
 
 		vector<Object>m_obj;
 		Event         m_evt;
@@ -122,27 +138,62 @@ public:
 	StreamsTestSuite() :
 		m_configFile("tests/streams/config.xml"),
 		m_config(m_configFile),
-		m_fakeParams(m_config.FindRef("application>module[name=\"FakeModule\"]", true)),
-		m_fakeModule(m_fakeParams)
+		m_fakeParams(m_config.FindRef("application>module[name=\"FakeModule\"]", true))
 		{}
 
 protected:
 	ConfigFile m_configFile;
 	ConfigReader        m_config;
-	ParameterStructure  m_fakeParams;
-	FakeModule          m_fakeModule;
+	Module::Parameters  m_fakeParams;
+	FakeModule*         mp_fakeModule1;
+	FakeModule*         mp_fakeModule2;
+	FakeModule*         mp_fakeModule3;
 
 public:
 	void setUp()
 	{
+		m_fakeParams.width = 320;
+		m_fakeParams.height = 240;
+		m_fakeParams.type = CV_8UC3;
+		mp_fakeModule1 = new FakeModule(m_fakeParams);
+		m_fakeParams.width = 160;
+		m_fakeParams.height = 120;
+		mp_fakeModule2 = new FakeModule(m_fakeParams);
+		m_fakeParams.width = 320;
+		m_fakeParams.height = 240;
+		mp_fakeModule3 = new FakeModule(m_fakeParams);
+
+		for(auto input : mp_fakeModule1->GetInputStreamList())
+		{
+			// connect inputs with outputs
+			int id = input.first;
+			mp_fakeModule2->RefInputStreamById(id).Connect(&mp_fakeModule1->RefOutputStreamById(id));
+			mp_fakeModule3->RefInputStreamById(id).Connect(&mp_fakeModule2->RefOutputStreamById(id));
+		}
 	}
 	void tearDown()
 	{
+		delete(mp_fakeModule1);
+		delete(mp_fakeModule2);
+		delete(mp_fakeModule3);
 	}
 
 public:
-	void testCreation()
+	void testConvertInput()
 	{
+		uint seed = 556345;
+
+		for(int i = 0 ; i < 100 ; i++)
+		{
+			mp_fakeModule1->ProcessFrame();
+			for(auto input : mp_fakeModule1->GetInputStreamList())
+			{
+				// TODO: This could be improved
+				int id = input.first;
+				mp_fakeModule2->RefInputStreamById(id).ConvertInput();
+				mp_fakeModule3->RefInputStreamById(id).ConvertInput();
+			}
+		}
 	}
 };
 #endif
