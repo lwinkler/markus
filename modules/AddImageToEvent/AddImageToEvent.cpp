2339bf0515d3f5e3a456909e9784c6a35e4ee3e3
Laurent Winkler <laurent.winkler@bluewin.ch>
2016-02-24 14:10:27 +0100

    Add module AddImageToEvent

---
+++ modules/AddImageToEvent/AddImageToEvent.cpp
@@ -0,0 +1,98 @@
+/*----------------------------------------------------------------------------------
+*
+*    MARKUS : a manager for video analysis modules
+*
+*    author : Laurent Winkler <lwinkler888@gmail.com>
+*
+*
+*    This file is part of Markus.
+*
+*    Markus is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Markus is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with Markus.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------------*/
+
+#include "AddImageToEvent.h"
+#include "StreamImage.h"
+#include "StreamEvent.h"
+#include "util.h"
+#include "Manager.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+log4cxx::LoggerPtr AddImageToEvent::m_logger(log4cxx::Logger::getLogger("AddImageToEvent"));
+
+AddImageToEvent::AddImageToEvent(ParameterStructure& xr_params):
+	Module(xr_params),
+	m_param(dynamic_cast<Parameters&>(xr_params)),
+	m_inputIm1(Size(m_param.width, m_param.height), m_param.type)
+	// m_inputIm2(Size(m_param.width, m_param.height), m_param.type)
+{
+	AddInputStream(0, new StreamEvent( "events", m_event, *this,     "Outgoing events"));
+	AddInputStream(1, new StreamImage( "input",  m_inputIm1, *this,   "Video input"));
+	// AddInputStream(2, new StreamImage( "input2", m_inputIm2, *this,   "Binary mask"));
+
+	AddInputStream(0, new StreamEvent( "output", m_event, *this,   "Outgoing events"));
+}
+
+AddImageToEvent::~AddImageToEvent()
+{
+}
+
+void AddImageToEvent::Reset()
+{
+	Module::Reset();
+
+	m_folder  = GetContext().GetOutputDir() + "/" + m_param.folder + "/";
+	SYSTEM("mkdir -p " + m_folder);
+	m_saveImage1 = m_inputStreams.at(1)->IsConnected();
+	// m_saveImage2 = m_inputStreams.at(2)->IsConnected();
+}
+
+void AddImageToEvent::ProcessFrame()
+{
+	const Object& obj(m_event.GetObject());
+	if(m_saveImage1)
+	{
+		std::stringstream ss1;
+		ss1 << m_folder << m_currentTimeStamp << "_" << m_event.GetEventName() << "_global_1." << m_param.extension;
+		addExternalImage(m_inputIm1, "globalImage", ss1.str(), m_event);
+
+		if(obj.width > 0 && obj.height > 0)
+		{
+			std::stringstream ss2;
+			ss2 << m_folder << m_currentTimeStamp << "_" << m_event.GetEventName() << "_" << obj.GetName()<< obj.GetId() << "_1" << "." << m_param.extension;
+			// cout<<"Save image "<<obj.m_posX<<" "<<obj.m_posY<<endl;
+			addExternalImage((m_inputIm1)(obj.GetRect()), "objectImage", ss2.str(), m_event);
+		}
+	}
+
+/*
+	if(m_saveImage2)
+	{
+		std::stringstream ss1;
+		ss1 << m_folder << m_currentTimeStamp << "_" << m_event.GetEventName() << "_global_2." << m_param.extension;
+		addExternalImage(m_inputIm2, "globalMask", ss1.str(), m_event);
+
+		if(obj.width > 0 && obj.height > 0)
+		{
+			std::stringstream ss2;
+			ss2 << m_folder << m_currentTimeStamp << "_" << m_event.GetEventName() << "_" << obj.GetName()<< obj.GetId() << "_2" << "." << m_param.extension;
+			// cout<<"Save image "<<obj.m_posX<<" "<<obj.m_posY<<endl;
+			addExternalImage((m_inputIm2)(obj.GetRect()), "objectMask", ss2.str(), m_event);
+		}
+	}
+	*/
+}
---
+++ modules/AddImageToEvent/AddImageToEvent.h
@@ -0,0 +1,81 @@
+/*----------------------------------------------------------------------------------
+*
+*    MARKUS : a manager for video analysis modules
+*
+*    author : Laurent Winkler <lwinkler888@gmail.com>
+*
+*
+*    This file is part of Markus.
+*
+*    Markus is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Markus is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with Markus.  If not, see <http://www.gnu.org/licenses/>.
+-------------------------------------------------------------------------------------*/
+
+#ifndef ADD_IMAGE_TO_EVENT_H
+#define ADD_IMAGE_TO_EVENT_H
+
+#include "Module.h"
+#include "Event.h"
+
+
+
+
+/**
+* @brief Add an attached image to an event (after saving it as a file)
+*/
+class AddImageToEvent : public Module
+{
+public:
+	class Parameters : public Module::Parameters
+	{
+	public:
+		Parameters(const ConfigReader& x_confReader) : Module::Parameters(x_confReader)
+		{
+			m_list.push_back(new ParameterString("folder_name" ,  "events_img"   , &folder    ,  "Name of the folder to create for images"));
+			m_list.push_back(new ParameterString("extension"  , "jpg"        , &extension , "Extension of the thumbnails. Determines the output format."));
+
+			RefParameterByName("type").SetRange("[CV_8UC1,CV_8UC3,CV_32FC1,CV_32FC3]");
+			RefParameterByName("extension").SetRange("[jpg,png]");
+
+			Init();
+		};
+		std::string folder;
+		std::string extension;
+	};
+
+	AddImageToEvent(ParameterStructure& xr_params);
+	~AddImageToEvent();
+	MKCLASS("AddImageToEvent")
+	MKCATEG("Output")
+	MKDESCR("Add an attached image to an event (after saving it as a file)")
+
+	virtual void ProcessFrame();
+	void Reset();
+
+private:
+	const Parameters& m_param;
+	static log4cxx::LoggerPtr m_logger;
+
+protected:
+	// input
+	cv::Mat m_inputIm1;
+	// cv::Mat m_inputIm2;
+	Event   m_event;
+
+	// temporary
+	bool m_saveImage1 = false;
+	// bool m_saveImage2 = false;
+	std::string m_folder;
+};
+
+#endif
